7e87db80f3f6dd8fc8076bae94190301e7ddf6eb
Ubuntu <ubuntu@ip-10-0-1-107.ap-south-2.compute.internal>
2026-09-08 08:35:38 +0000

    initial terraform commit

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "")
+SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 DEBUG = False
 
